ae351fea9a83d82013e78d99e8463a4eb5a0030e
kinghao <wang_yi@163.com>
2022-10-12 11:43:16 +0800

    修复百度视频加载失败问题

--- src/libs/ads/channel/baidu/index.js
+++ src/libs/ads/channel/baidu/index.js
@@ -1,11 +1,12 @@
 
-const union_quick_app_sdk = getGlobalData('App').$def.union_quick_app_sdk
 /**
  * baidu
  */
 export default {
     // 创建激励视频
     createRewardedVideoAd(obj) {
+        console.info(getGlobalData('App'))
+        const { union_quick_app_sdk } = getGlobalData('App').$def
         return union_quick_app_sdk.createRewardedVideoAd(obj)
     }
 }
