9b44ceaae78a55404bacbccf8ceeedfccce45641
kinghao <wang_yi@163.com>
2022-10-12 17:26:05 +0800

    删除无用代码

--- src/libs/ads/channel/oppo/index.js
+++ src/libs/ads/channel/oppo/index.js
@@ -1,34 +1,41 @@
-
 const ad = require('@service.ad')
 /**
  * oppo厂商
  */
 export default {
-    // 创建激励视频
-    createRewardedVideoAd(obj) {
-        let reward = ad.createRewardedVideoAd(obj)
-        return reward
-    },
-    // Banner广告
-    createBannerAd(obj) {
-        return ad.createBannerAd(obj)
-    },
-    // 插屏广告
-    createInterstitialAd(obj) {
-        return ad.createInterstitialAd(obj)
-    },
-    // 原生 native 广告
-    createNativeAd(obj) {
-        return ad.createNativeAd(obj)
-    },
-    // 原生广告，按照全局原生接口名称
-    preloadAd({ adUnitId, success, fail }) {
-        let nativeAd = ad.createNativeAd({ adUnitId })
-        nativeAd.load()
-        nativeAd.onLoad(function (res) {
-            success(res)
-        }, (err) => {
-            fail(err)
-        })
-    }
+  // 创建激励视频
+  createRewardedVideoAd(obj) {
+    let reward = ad.createRewardedVideoAd(obj)
+    return reward
+  },
+  // Banner广告
+  createBannerAd(obj) {
+    return ad.createBannerAd(obj)
+  },
+  // 插屏广告
+  createInterstitialAd(obj) {
+    return ad.createInterstitialAd(obj)
+  },
+  // 原生 native 广告
+  createNativeAd(obj) {
+    return ad.createNativeAd(obj)
+  },
+  // 原生广告，按照全局原生接口名称
+  preloadAd({ adUnitId, success, fail }) {
+    let nativeAd = ad.createNativeAd({ adUnitId })
+    nativeAd.load()
+    nativeAd.onLoad(
+      function (res) {
+        success(res)
+      },
+      (err) => {
+        fail(err)
+      },
+    )
+    nativeAd.onError(
+      function (err) {
+        fail(err)
+      },
+    )
+  },
 }
--- src/libs/ads/channel/vivo/index.js
+++ src/libs/ads/channel/vivo/index.js
@@ -1,44 +1,24 @@
-
 const ad = require('@service.ad')
 /**
  * vivo厂商
  */
 export default {
-    // 创建激励视频
-    createRewardedVideoAd(obj) {
-        return ad.createRewardedVideoAd(obj)
-    },
-    // Banner广告
-    createBannerAd(obj) {
-        return ad.createBannerAd(obj)
-    },
-    // 插屏广告
-    createInterstitialAd(obj) {
-        return ad.createInterstitialAd(obj)
-    },
-    preloadAd(obj) {
-        return ad.preloadAd(obj)
-    },
-    // 原生 native 广告
-    createNativeAd(obj) {
-        return {
-            load() {
-                
-            },
-            onLoad(callBack) {
-                ad.preloadAd({
-                    ...obj,
-                    success: (res) => {
-                        console.info(res)
-                        callBack(res)
-                    }, fail: (res, code) => {
-                        console.info(res)
-                    }
-                })
-            },
-            onError() {
-                console.info(2222)
-            }
-        }
-    }
+  // 创建激励视频
+  createRewardedVideoAd(obj) {
+    console.info(obj)
+    return ad.createRewardedVideoAd(obj)
+  },
+  // Banner广告
+  createBannerAd(obj) {
+    return ad.createBannerAd(obj)
+  },
+  // 插屏广告
+  createInterstitialAd(obj) {
+    return ad.createInterstitialAd(obj)
+  },
+  preloadAd(obj) {
+    return ad.preloadAd(obj)
+  },
+  // 原生 native 广告
+  createNativeAd(obj) {},
 }
